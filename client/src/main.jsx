fb82b4e8c8a6a299f8584847b41f1e4f79d43f64
Kanchan Singh <kanchan.matulsi@gmail.com>
2025-08-10 15:35:21 +0530

    working version without css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+window.process = { env: { NODE_ENV: 'development' } }
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
